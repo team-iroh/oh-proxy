bf76e1260605bdfeb3ca3c073e366bbc047c540b
Orlando <jcsf1991@gmail.com>
2020-07-02 17:15:25 -0700

    fixing poxy server bugs

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
 
 app.use(express.static(__dirname + '/../public/lib'));
+app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('/', (req, res) => res.send(''))
+app.use('/api/story/',
+  createProxyMiddleware({ target: 'http://localhost:3003/', changeOrigin: true }));
+app.use('/api/RisksAndChallenges/',
+  createProxyMiddleware({ target: 'http://localhost:3003/', changeOrigin: true }));
+app.use('/api/EnvironmentalCommitments/',
+  createProxyMiddleware({ target: 'http://localhost:3003/', changeOrigin: true }));
+
+
+app.get('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/lib/', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
